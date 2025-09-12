0cb1ed97398e9b73f4c46011891b74d6b6b05cd8
Jeffery Fang <jefferyfang324@gmail.com>
2025-09-12 19:05:02 -0400

    added more linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import unUsedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -25,10 +26,24 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      'unused-imports': unUsedImports,
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': 'warn'
     },
   },
 );
