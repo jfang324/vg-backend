5178d0f2471f7b835ea83b563219d0612a9db42b
Jeffery Fang <jefferyfang324@gmail.com>
2025-10-24 22:45:47 -0400

    updated readme

--- src/integrations/henrik-dev/schemas/ability-casts.schema.ts
+++ src/integrations/henrik-dev/schemas/ability-casts.schema.ts
@@ -1,10 +1,22 @@
 import { z } from 'zod'
 
 export const AbilityCastsSchema = z.object({
-	grenade: z.number().nullable().transform((value) => value ?? 0),
-	ability1: z.number().nullable().transform((value) => value ?? 0),
-	ability2: z.number().nullable().transform((value) => value ?? 0),
-	ultimate: z.number().nullable().transform((value) => value ?? 0)
+	grenade: z
+		.number()
+		.nullable()
+		.transform((value) => value ?? 0),
+	ability1: z
+		.number()
+		.nullable()
+		.transform((value) => value ?? 0),
+	ability2: z
+		.number()
+		.nullable()
+		.transform((value) => value ?? 0),
+	ultimate: z
+		.number()
+		.nullable()
+		.transform((value) => value ?? 0)
 })
 
 export type AbilityCasts = z.infer<typeof AbilityCastsSchema>
