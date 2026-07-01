9974a98453810f34518d35dfba4662e55b221cf6
Jeffery Fang <jefferyfang324@gmail.com>
2026-07-01 00:33:19 -0400

    fix: updated logging service tests to align with new parameters

--- src/modules/logging/logging.service.spec.ts
+++ src/modules/logging/logging.service.spec.ts
@@ -1,65 +1,73 @@
 import { mockLogger } from '@mocks/nest/nest.mock'
-import { Logger } from '@nestjs/common'
-import { AxiosError } from 'axios'
 import { LoggingService } from './logging.service'
 
 describe('LoggingService', () => {
 	let loggingService: LoggingService
 
 	beforeEach(() => {
-		loggingService = new LoggingService(new Logger())
+		loggingService = new LoggingService()
 	})
 
 	afterEach(() => {
 		jest.clearAllMocks()
 	})
 
-	it('should log log the message', () => {
+	it('should log the message', () => {
 		const message = 'test message'
 		loggingService.logInfo(message)
 
 		expect(mockLogger.log).toHaveBeenCalledWith(message)
 	})
 
-	it('should log the error message and status code', () => {
-		const serviceName = 'test service'
-		const apiError = new AxiosError('test error', '500')
+	it('should log the error message and API context', () => {
+		const caller = 'testCaller'
+		const api = 'testApi'
+		const error = new Error('test error')
 
-		loggingService.logApiError(serviceName, apiError)
+		loggingService.logApiError(caller, api, error)
 
-		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(serviceName))
-		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(apiError.message))
+		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(caller))
+		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(api))
+		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(error.message))
 	})
 
-	it('should log the error message and data source', () => {
+	it('should log the error message and validation context', () => {
+		const caller = 'testCaller'
 		const dataSource = 'test data source'
 		const error = 'test error'
 
-		loggingService.logValidationError(dataSource, error)
+		loggingService.logValidationError(caller, dataSource, error)
 
+		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(caller))
 		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(dataSource))
 		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(error))
 	})
 
-	it('should log the error message and repository', () => {
+	it('should log the error message and database context', () => {
+		const caller = 'testCaller'
 		const repository = 'test repository'
 		const operation = 'test operation'
 		const error = 'test error'
 
-		loggingService.logDatabaseError(repository, operation, error)
+		loggingService.logDatabaseError(caller, repository, operation, error)
 
+		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(caller))
 		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(repository))
 		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(operation))
 		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(error))
 	})
 
-	it('should log the error message and source', () => {
+	it('should log the error message and Redis context', () => {
+		const caller = 'testCaller'
 		const source = 'test source'
+		const operation = 'testOperation'
 		const error = 'test error'
 
-		loggingService.logRedisError(source, error)
+		loggingService.logRedisError(caller, source, operation, error)
 
+		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(caller))
 		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(source))
+		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(operation))
 		expect(mockLogger.error).toHaveBeenCalledWith(expect.stringContaining(error))
 	})
 })
