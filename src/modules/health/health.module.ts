1fcaab4d01e252a66edb1d6098f155dd41e4e2b9
Jeffery Fang <jefferyfang324@gmail.com>
2026-07-02 23:17:09 -0400

    refactor: updated health check to use config service instead of directly using env variables

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { HealthController } from './health.controller'
 import { HealthService } from './health.service'
 
 @Module({
+	imports: [ConfigModule],
 	controllers: [HealthController],
 	providers: [HealthService]
 })
